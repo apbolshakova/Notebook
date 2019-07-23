960fd6e654664b0330e2df7757c9209de090eff1
julody <julody@mail.ru>
2019-07-23 22:29:37 +0300

    Рефакторинг и отладка класса Notebook

--- js/notebook.js
+++ js/notebook.js
@@ -1,12 +1,17 @@
 class Notebook {
   constructor() {
     //TODO: delete testing values
-    localStorage.setItem("notesNum", 2);
+    localStorage.setItem("notesNum", 1);
     localStorage.setItem("note0", "first one");
-    localStorage.setItem("note1", "second one");
-    
+    this.handleStoredNotes();
+  }
+  handleStoredNotes() {
     this.notesNum = localStorage.getItem("notesNum") || 0;
-    for(let i = 0; i < this.notesNum; i++) {
+    if (this.notesNum < 0) {
+      localStorage.setItem("notesNum", 0);
+      this.notesNum = 0;
+    }
+    for (let i = 0; i < this.notesNum; i++) {
       let noteText = localStorage.getItem("note" + i);
       this.renderNote(i, noteText);
     }
@@ -14,13 +19,12 @@ class Notebook {
   renderNote(i, noteText) {
     let notesContainer = document.getElementById("notesContainer");
     let noteTemplate = document.getElementById("noteTemplate");
-    let note = noteTemplate.cloneNode(true);
     
+    let note = noteTemplate.cloneNode(true);
     note.classList.remove("invisible");
     note.id = "note" + i;
     let noteInput = note.getElementsByClassName("note-container__note-input");
     noteInput[0].value = noteText;
-    console.log(noteInput[0]);
     notesContainer.insertBefore(note, notesContainer.children[0]);
   }
 }
